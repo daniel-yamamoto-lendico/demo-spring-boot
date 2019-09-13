bd2d78bea7eed7d2ec8c254a108c70a42502b01e
Daniel Yamamoto <daniel.yamamoto@lendico.de>
2019-09-13 17:22:02 +0200

    adding users now working

--- src/main/java/com/example/demo/controllers/UserController.java
+++ src/main/java/com/example/demo/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.example.demo.controllers;
 
+import com.example.demo.entities.Role;
 import com.example.demo.entities.User;
+import com.example.demo.repositories.RoleRepository;
 import com.example.demo.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
@@ -13,6 +18,9 @@ public class UserController {
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private RoleRepository roleRepository;
+
     @GetMapping("/findAll")
     public ResponseEntity getUsers() {
         return ResponseEntity.ok().body(userRepository.findAll());
@@ -20,6 +28,13 @@ public class UserController {
 
     @PutMapping("/insert")
     public ResponseEntity insertUser(@RequestBody User user) {
+        List<Role> r = new ArrayList<>(), roles = user.getRoles();
+        for (Role role : roles)
+        {
+            role = roleRepository.findByRoleName(role.getRoleName());
+            r.add(role);
+        }
+        user.setRoles(r);
         return ResponseEntity.accepted().body(userRepository.save(user));
     }
 }
